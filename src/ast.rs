b2c8001e59aa71e8204fefe3e319c0daf046ef2e
Oliver Reardon-Smith <or1426@my.bristol.ac.uk>
2015-07-14 11:59:19 +0100

    Add (quite bad) support for parsing AST from string input

--- src/ast.rs
+++ src/ast.rs
@@ -1,5 +1,6 @@
 use std::string;
 use std::vec;
+use std::result;
 
 pub enum Ast{
     Token(string::String),
@@ -7,6 +8,57 @@ pub enum Ast{
 }
 
 impl Ast{
+    pub fn from_string(s: string::String) -> result::Result<Ast, &'static str> {
+        //First add spaces to ensure brackets end up as tokens by themselves
+        let mut s = s.replace("(", " ( ");
+        s = s.replace(")", " ) ");
+        let str = s.trim();
+        let mut v: vec::Vec<string::String> = str.split(" ").map(|s| s.to_string()).collect();
+        
+        v.reverse();
+        match v.pop() {
+            None => return Err("Needs some tokens..."),
+            Some(str) => match &str[..]{
+                "(" => (),
+                _ => {
+                    return Err("First token must be \"(\".")},
+            },
+        };
+        match Ast::from_string_vec(v){
+            Ok((_, ast)) => Ok(ast),
+            Err(e) => Err(e),
+        }
+    }
+
+    fn from_string_vec(mut v : vec::Vec<string::String>) -> result::Result<(vec::Vec<string::String>, Ast), &'static str> {
+        let mut ast_vec = vec![];
+        loop{
+            match v.pop() {
+                None => break,
+                Some(str) => match &str[..] {
+                    ")" => return Ok((v, Ast::SubList(ast_vec))),
+                    "(" => {
+                        match Ast::from_string_vec(v.clone()) {
+                            Ok((v_ret, ast)) => {
+                                ast_vec.push(ast);
+                                v = v_ret;
+                            },
+                            Err(s) => return Err(s),
+                        }
+                    },
+                    "" => (),
+                    " " => (), 
+                    _ => {
+                        //println!("Debug: {}", str);
+                        ast_vec.push(Ast::Token(str.to_string()))
+                    },
+                }   
+            };
+        };
+
+        Err("Seem to have insufficient closing brackets to parse ast!")
+    }
+
     pub fn to_string(self: Ast) -> string::String {
         self.to_string_with_indent(0)
     }
@@ -14,7 +66,7 @@ impl Ast{
     fn to_string_with_indent(self: Ast, indent: i32) -> string::String {
         let mut output = "".to_string();
         
-        match self{
+        match self {
             Ast::Token(t) => output.push_str(&t[..]),
             Ast::SubList(v) => {
                 output.push_str("\n");
@@ -25,6 +77,7 @@ impl Ast{
                 for element in v {
                     output.push_str(&element.to_string_with_indent(indent+1)[..]);
                 }
+                output.pop(); //don't want the ugly extra whitespace
                 output.push_str(")");
             },
         };
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod ast;
 
 fn main() {
-    let a = ast::Ast::SubList(vec![ast::Ast::Token("Hello".to_string()),
-                                   ast::Ast::SubList(vec![ast::Ast::Token("Wo".to_string()),ast::Ast::Token("rl".to_string()), ast::Ast::Token("d!".to_string())])]);
-    println!("{}", a.to_string());
+    //let a = ast::Ast::SubList(vec![ast::Ast::Token("Hello".to_string()),
+    //                               ast::Ast::SubList(vec![ast::Ast::Token("Wo".to_string()),ast::Ast::Token("rl".to_string()), ast::Ast::Token("d!".to_string())])]);
+    match ast::Ast::from_string("(Hello ( world! ) (and any (other worlds! )) )".to_string()){
+        Ok(a) => println!("{}", a.to_string()),
+        Err(e) => println!("{}", e),
+    };    
 }
