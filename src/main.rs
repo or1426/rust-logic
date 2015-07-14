f2d9409f354966c03bd59b3f66e536502884b866
Oliver Reardon-Smith <or1426@my.bristol.ac.uk>
2015-07-14 12:24:10 +0100

    Added simple rpl with linenoise library

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
+extern crate linenoise;
+use std::string;
 mod ast;
 
 fn main() {
-    //let a = ast::Ast::SubList(vec![ast::Ast::Token("Hello".to_string()),
-    //                               ast::Ast::SubList(vec![ast::Ast::Token("Wo".to_string()),ast::Ast::Token("rl".to_string()), ast::Ast::Token("d!".to_string())])]);
-    match ast::Ast::from_string("(Hello ( world! ) (and any (other worlds! )) )".to_string()){
-        Ok(a) => println!("{}", a.to_string()),
-        Err(e) => println!("{}", e),
-    };    
+     loop {
+         let val = linenoise::input(">>> ");
+        match val {
+            None => { break }
+            Some(input) => {
+                linenoise::history_add(&input[..]);
+                match ast::Ast::from_string(input){
+                    Ok(a) => println!("<<< {}", a.to_string()),
+                    Err(e) => println!("!!! {}", e),
+                };
+            }
+        }
+    }
 }
