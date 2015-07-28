0571a91a47b835bccbc4e3003b510c03b2f00165
Oliver Reardon-Smith <or1426@my.bristol.ac.uk>
2015-07-28 23:08:47 +0100

    Added Clone implementation for LFn struct

--- src/lval/lfunc/lfn.rs
+++ src/lval/lfunc/lfn.rs
@@ -7,10 +7,23 @@ use env;
 use std::vec;
 use std::option;
 
-pub struct LFn {
+pub struct LFn{
     tree: ast::Ast,
     environment: env::Env;
     sig: vec::Vec<ltype::LType>,
     ret_type: ltype::LType,
     applied_args: vec::Vec<lval::LVal>
+}
+
+
+impl Clone for LFn{
+    fn clone(&self)->LFn{
+        LFn{
+            tree:self.tree.clone(),
+            environment:self.environment.clone(),
+            sif:self.sig.clone(),
+            ret_type:self.ret_type.clone(),
+            applied_args:self.applied_args.clone(),
+        }
+    }
 }
